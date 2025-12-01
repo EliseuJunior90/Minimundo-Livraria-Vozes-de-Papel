d089d191660ae4a214b3027d441f694f7c462b58
EliseuJunior90 <jotta.oliver33@gmail.com>
2025-11-30 22:50:34 -0300

    praticando comandos delete na tabela

---
+++ 05_deleteOnTable.sql
@@ -0,0 +1,11 @@
+--remover cliente
+DELETE FROM cliente
+WHERE id_cliente = 3;
+
+---remover Autor
+DELETE FROM autor
+WHERE nome = 'Roberto Macarrão';
+
+--remover editora
+DELETE FROM editora
+WHERE id_editora = 2;
